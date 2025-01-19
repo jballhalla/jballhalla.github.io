17c675c3e40ccfadb7037386388309beefa14f8a
jballhalla <91580027+jballhalla@users.noreply.github.com>
2025-01-19 07:35:20 +0000

    Deploying to gh-pages from @ jballhalla/jballhalla.github.io@b5e1ac1acba1fd87a11082308a890ce397cdb24d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,7 +51,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "post-truth-after-tsvetaeva",
+        },{id: "post-what-comes-after-irony",
+      
+        title: "What comes after irony?",
+      
+      description: "Rorty&#39;s iconoclastic &quot;ironist&quot; runs into trouble when they want to create something people can act on.",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/What-comes-after-irony/";
+        
+      },
+    },{id: "post-truth-after-tsvetaeva",
       
         title: "Truth after Tsvetaeva",
       
