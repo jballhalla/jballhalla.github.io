45a304d7cbfa351f04b323d87b206a876773919b
jballhalla <91580027+jballhalla@users.noreply.github.com>
2025-02-14 08:02:38 +0000

    Deploying to gh-pages from @ jballhalla/jballhalla.github.io@ff1d40d75956c1aeda14f1de3aeaa5e9f55bcfa5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,7 +51,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "post-on-harvey-39-s-justice-nature-amp-the-geography-of-difference",
+        },{id: "post-staying-with-the-grain",
+      
+        title: "Staying with the Grain",
+      
+      description: "For Margaret Lanterman (portrayed by Catherine E. Coulson), an American sibyl.",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/Staying-with-the-Grain/";
+        
+      },
+    },{id: "post-on-harvey-39-s-justice-nature-amp-the-geography-of-difference",
       
         title: "On Harvey&#39;s Justice, Nature &amp; the Geography of Difference",
       
